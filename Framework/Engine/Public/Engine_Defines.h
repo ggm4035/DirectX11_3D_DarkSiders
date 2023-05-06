08cffefbfcfa2a1b383d98521b46f108834620fe
ggm4035 <110024770+ggm4035@users.noreply.github.com>
2023-05-06 12:22:46 +0900

    Framework

---
+++ Framework/Engine/Public/Engine_Defines.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <d3d11.h>
+#include <DirectXMath.h>
+
+using namespace DirectX;
+
+#include "Engine_Function.h"
+#include "Engine_TypeDef.h"
+#include "Engine_Macro.h"
+#include "Engine_Struct.h"
+
+using namespace Engine;
+
+
